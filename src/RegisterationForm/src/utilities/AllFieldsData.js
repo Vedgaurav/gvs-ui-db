6abf7106dbbe7f32c285d972628fef25e99a07a8
Gaurav918 <gauravsingh181997@gmail.com>
2023-04-13 21:14:43 +0530

    changes to be done

--- src/RegisterationForm/src/utilities/AllFieldsData.js
+++ src/RegisterationForm/src/utilities/AllFieldsData.js
@@ -1,53 +1,57 @@
 export const requiredDataAllFields={
-    devId: null,
+    
     fname: "",
     mname: "",
     lname: "",
     initiatedName: "",
     gender: "Male",
-    dob: "2023-02-23 00:00:00",
+    odob: "2023-02-23",
+    cdob: "2023-02-23",
     caste:"",
     gotra:"",
-    maritialStatus: null,
-    aspiringAshram: null,
-    bloodGroup: null,
-    language: null,
-    profileImgUrl: null,
+    maritialStatus: "",
+    aspiringAshram: "",
+    bloodGroup: "A +ve",
+    language: ["HINDI"],
+    profileImgUrl: "",
     primaryPhone: '',
     whatsappPhone: '',
     email: '',
-    currentAddress: '',
-    permanentAddress: "",
-    centerConnectedTo: null,
-    facilitator: null,
-    counselor: null,
-    spiritualMaster: null,
-    chantingRounds: "16",
-    yearChantingSince: null,
-    yearChanting16Rounds: null,
-    introducedBy: null,
-    yearOfIntroduction: null,
-    placeIntroducedIn: null,
+    currentAddress: {line1:'',line2:'',
+    country:'India',
+    state:'Delhi',
+    city:'Delhi',
+    pinCode:''},
+    permanentAddress: {line1:'',line2:'',
+    country:'India',
+    state:'Delhi',
+    city:'Delhi',
+    pinCode:''},
+    connectedTemple: '',
+    facilitator: "",
+    counselor: "",
+    spiritualMaster: "",
+    chantingRounds: "",
+    yearChantingSince: "",
+    yearChanting16Rounds: "",
+    introducedBy: "",
+    yearOfIntroduction: "",
     previousCounselor: null,
     preferredServices: null,
     servicesRendered: null,
-    remarks: null,
     education: null,
     occupation: null,
     presentDesignation: null,
-    awards: null,
     skills: null,
     currentCompany: null,
-    officeAddress: null,
-    previousReligion: null,
+    officeLocation: null,
     birthCity: null,
     birthState: null,
     motherTongue: null,
     fathersName: null,
     mothersName: null,
-    parent: false,
     modified: false,
-    stayingInHaldiaVoice: false,
+    priviledge:"",
     isValidFname: false,
     isValidGender:false,
     isvalidAshrama:false,
@@ -56,6 +60,62 @@ export const requiredDataAllFields={
     isValidImageURL:false,
     isSubmitDisabled:true,
     isValidPrimaryNo :false,
-    isValidEmail:false
+    isValidEmail:false,
+    isValidCenterConnectedTo:false
   }
-  
+  //      UNMARRIED,
+//      MARRIED,
+//      DIVORCED,
+//      BRAHMACHARI,
+//      SANYASI
+//ashrama
+//  BRAHMACHARI,
+//      GRIHASTHA,
+//      NOT_DECIDED
+
+//language: send an array
+// BENGALI,
+//     ENGLISH,
+//     HINDI,
+//     GUJARATI,
+//     KANNADA,
+//     MARATHI,
+//     MAITHILI,
+//     ORIYA,
+//     NEPALI,
+//     PUNJABI
+
+//Education
+// NO_EDUCATION,
+//     PRE_PRIMARY_SCHOOL,
+//     PRIMARY_SCHOOL,
+//     MIDDLE_SCHOOL,
+//     SECONDARY_SCHOOL,
+//     HIGHER_SECONDARY_SCHOOL,
+//     DIPLOMA,
+//     UG,
+//     PG,
+//     DOCTORATE,
+//     POST_DOCTORATE
+
+//Occupation
+// EMPLOYEED_FULL_TIME,
+//     EMPLOYEED_PART_TIME,
+//     SELF_EMPLOYED,
+//     UNEMPLOYED,
+//     HOMEMAKER,
+//     RETIRED,
+//     STUDENT
+
+//priviledge
+// SUPER_ADMIN,
+//     ADMIN,  
+//     GUARDIAN, 
+//     VOLUNTEER,
+//     USER
+
+// Personal 
+// Family
+// Devotional
+// Professional
+// Contact
