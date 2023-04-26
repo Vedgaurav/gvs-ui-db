4826d9025d2acdae729a646186d662cc748cfc3e
Gaurav918 <gauravsingh181997@gmail.com>
2023-04-26 22:11:06 +0530

    corrected address regex

--- src/RegisterationForm/src/RegexExpsValidation/RegexExps.js
+++ src/RegisterationForm/src/RegexExpsValidation/RegexExps.js
@@ -2,4 +2,4 @@ export const validateName ="^[a-zA-Z][a-zA-Z .,'-]*$";
 export const validatePhoneNo="^[0-9]{10}$";
 export const validateRounds="^[0-9]{1,3}$";
 export const validateEmail='[a-z0-9]+@[a-z]+\.[a-z]{2,3}$';
-export const validateAddress="^[#.0-9a-zA-Z\s,-]+$";
+export const validateAddress="^[a-zA-Z0-9][a-zA-Z0-9 .,'-]*$";
